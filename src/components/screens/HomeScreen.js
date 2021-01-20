3169d0c933def961b13b2e5fd97b626913c26bd4
Calvin <63000172+calvintettey@users.noreply.github.com>
2021-01-20 14:49:54 +0000

    building footer on home screen

--- src/components/screens/HomeScreen.js
+++ src/components/screens/HomeScreen.js
@@ -1,94 +1,133 @@
-import React from 'react'
+import React from "react";
 import {
-    SafeAreaView,
-    Dimensions,
-    StyleSheet,
-    Text,
-    TextInput,
-    View,
-    Image,
-    TouchableOpacity,
-  } from "react-native";
-  import { QRCode } from 'react-native-custom-qr-codes-expo';
+  SafeAreaView,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+  TouchableOpacity,
+  Button,
+} from "react-native";
+import { QRCode } from "react-native-custom-qr-codes-expo";
 
-  const { width, height } = Dimensions.get("screen");
-  export default function HomeScreen() {
-    return (
-        <View style={styles.container}>
-            <View style={styles.textBox}>
-                <Text style={styles.headText}>Exchange Contact Information</Text>
-                <Text style={styles.subText}>Scan this QR below to share your contacts</Text>
-            </View>
+const { width, height } = Dimensions.get("screen");
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.body}>
+        <View style={styles.textBox}>
+          <Text style={styles.headText}>Exchange Contact Information</Text>
+          <Text style={styles.subText}>
+            Scan this QR below to share your contacts
+          </Text>
+        </View>
 
-            <View style={styles.qrBox}>
-            <QRCode codeStyle='square' content='https://reactnative.com'/>
-            </View>
+        <View style={styles.qrBox}>
+          <QRCode codeStyle="square" content="https://reactnative.com" />
+        </View>
 
-            <View style={styles.imageBox}>
-        <Image
-          style={styles.image}
-          source={require("../../../assets/mydetails.jpg")}
-        />
+        <View style={styles.profileBox}>
+          <Image
+            style={styles.image}
+            source={require("../../../assets/mydetails.jpg")}
+          />
 
-        <View style={styles.nameroleBox}>
-          <Text style={styles.nameText}>Larry Hutchson</Text>
-          <Text style={styles.roleText}>Sales Executive</Text>
+          <View style={styles.nameroleBox}>
+            <Text style={styles.nameText}>Larry Hutchson</Text>
+            <Text style={styles.roleText}>Sales Executive</Text>
+          </View>
         </View>
       </View>
-        </View>
-    )
+
+      <View style={styles.footer}>
+        <View style={styles.footerBar}></View>
+        <Text style={{ fontSize: 16, color:"black" }}>Want to add a new connection?</Text>
+        <TouchableOpacity style={styles.scanBtn}>
+          <Text style={{ fontSize: 16, color:"#d91139" }}>Scan QR</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: "column",
-        backgroundColor: "#fcefcc",
-        paddingHorizontal: 0.06 * width,
-    },
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#fcefcc",
+    justifyContent: "space-between",
+  },
 
-    textBox:{
-        
-        marginTop: 0.1 * height,
-    },
+  body: {
+    paddingHorizontal: 0.06 * width,
+  },
 
-    headText:{
-        fontSize: 16
-    },
+  textBox: {
+    marginTop: 0.1 * height,
+  },
 
-    subText:{
-        fontSize:16,
-        color:"grey"
-    },
+  headText: {
+    fontSize: 16,
+  },
 
-    qrBox:{
-        marginVertical: 0.08 * height,
-    },
+  subText: {
+    fontSize: 16,
+    color: "grey",
+  },
 
-    imageBox: {
-        flexDirection: "row",
-        alignItems: "center",
-        // marginTop: 0.1 * height,
-      },
-    
-      image: {
-        height: 0.08 * height,
-        width: 0.17 * width,
-        borderRadius: 100,
-      },
-    
-      nameroleBox: {
-        marginLeft: 0.04 * width,
-      },
-    
-      nameText: {
-        fontSize: 16,
-      },
-    
-      roleText: {
-        fontSize: 16,
-        color: "grey",
-      },
+  qrBox: {
+    marginVertical: 0.08 * height,
+    alignSelf: "center",
+  },
 
+  profileBox: {
+    flexDirection: "row",
+    alignItems: "center",
+    // marginTop: 0.1 * height,
+  },
 
-})
+  image: {
+    height: 0.08 * height,
+    width: 0.17 * width,
+    borderRadius: 100,
+  },
+
+  nameroleBox: {
+    marginLeft: 0.04 * width,
+  },
+
+  nameText: {
+    fontSize: 16,
+  },
+
+  roleText: {
+    fontSize: 16,
+    color: "grey",
+  },
+
+  footer: {
+    width: 1 * width,
+    flexDirection: "row",
+    height: 0.12 * height,
+    alignItems: "flex-start",
+    // justifyContent: "space-around",
+    alignItems: "center",
+    paddingHorizontal: 0.06 * width,
+    borderColor: "#adadad",
+    borderTopWidth: 1,
+    // backgroundColor: "red",
+  },
+
+  scanBtn:{
+    marginLeft: 0.03 * width,
+    borderColor:"#d91139",
+    borderRadius:3,
+    borderWidth:1,
+    height: 0.04 * height,
+    width: 0.3 * width,
+    justifyContent: "center",
+    alignItems: "center",
+  }
+});
---
+++ src/components/screens/QRScannerScreen.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {
+    SafeAreaView,
+    Dimensions,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TouchableOpacity,
+  } from "react-native";
+  
+const { width, height } = Dimensions.get("screen");
+export default function QRScannerScreen() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
